d801effec90a9f8d4a7f5abbda48dd1ed3ac1bc3
Hamza Hussein <hussei91@uwindsor.ca>
2026-01-13 14:17:25 -0500

    - added "version info" box to provide clarity on what will be added to, changed about, or removed from the site - Breakdown - The Alan Parsons Project

--- js/notice.js
+++ js/notice.js
@@ -1,27 +1,52 @@
 const notice = document.getElementsByClassName("notice")[0];
 const noticeBtn = document.getElementById("notice-button");
-const anchor    = document.getElementById("notice-anchor");
+const noticeCBox = document.getElementById("disableNotice");
+// const anchor    = document.getElementById("notice-anchor");
 const hasSeenNotice = localStorage.getItem("hasSeenNotice");
 // localStorage.clear("hasSeenNotice");
-if (hasSeenNotice) {
+console.log(localStorage.getItem("hasSeenNotice"));
+
+const versionBtn = document.getElementById("version-button");
+const versionInfo = document.getElementById("version-screen");
+if (localStorage.getItem("hasSeenNotice") === "true") {
     console.log("notice button already clicked!");
+
+    setTimeout(() => {
+        versionBtn.style.opacity = 1;
+        console.log("revealed version button");
+    }, 3000);
 }
 else {
     notice.style.display = "grid";
-    anchor.addEventListener("click", function() {
-        localStorage.setItem("hasSeenNotice", "true");
-        // alert("button status saved");
-        notice.classList.add("fadeOut");
-        setTimeout(() => {
-            notice.style.display = "none";
-        }, 500);
-    });
+
     noticeBtn.addEventListener("click", function() {
-        localStorage.setItem("hasSeenNotice", "true");
-        // alert("button status saved");
+        if(noticeCBox.checked){
+            localStorage.setItem("hasSeenNotice", "true");
+            console.log("checkbox is checked");
+        }
+        else{
+            localStorage.setItem("hasSeenNotice", "false");
+            console.log("checkbox is NOT checked");
+        }
         notice.classList.add("fadeOut");
         setTimeout(() => {
             notice.style.display = "none";
         }, 500);
+        setTimeout(() => {
+            versionBtn.style.opacity = 1;
+            console.log("revealed version button");
+        }, 3000);
     });
+}
+
+function revealVersionInfo() {
+    versionInfo.style.display = "flex";
+    versionInfo.style.opacity = 1;
+}
+
+function hideVersionInfo() {
+    versionInfo.style.opacity = 0;
+    setTimeout(() => {
+        versionInfo.style.display = "none";
+    }, 1500);
 }
